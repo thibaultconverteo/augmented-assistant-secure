1562b32d52eef53a69b9345f807d8fb9c536d330
Loris Dié <loris.die@converteo.com>
2024-02-29 11:11:28 +0100

    [webapp] hard fix url issue

--- webapp/data/data.ts
+++ webapp/data/data.ts
@@ -1,6 +1,6 @@
 export async function getData(prompt: string | undefined) {
   const res = await fetch(
-    process.env.NEXT_PUBLIC_API_URL + "/processPrompt",
+    "https://augmented-chatbot-demo-4o52ykz34a-ew.a.run.app/processPrompt",
 
     {
       method: "POST",
