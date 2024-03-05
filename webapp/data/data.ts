5b01e34b6bc07f5561ff5c80e8c7434f2806608d
Loris Dié <loris.die@converteo.com>
2024-03-05 13:21:04 +0100

    [webapp] get/send sessionId ready

--- webapp/data/data.ts
+++ webapp/data/data.ts
@@ -1,4 +1,28 @@
+//Waiting API to have sessionId parameter in the response
+// let sessionId = "";
+
 export async function getData(prompt: string | undefined) {
+  const chatHistory = localStorage.getItem("chat_history");
+  if (chatHistory === null) {
+    const res = await fetch(
+      "https://augmented-chatbot-demo-4o52ykz34a-ew.a.run.app/processPrompt",
+
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: prompt?.toLowerCase() }),
+      }
+    );
+
+    const data = await res.json();
+    //Waiting API to have sessionId parameter in the response
+    // sessionId = data.sessionId ;
+
+    return data;
+  }
+
   const res = await fetch(
     "https://augmented-chatbot-demo-4o52ykz34a-ew.a.run.app/processPrompt",
 
@@ -6,12 +30,17 @@ export async function getData(prompt: string | undefined) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        //Waiting API to have sessionId parameter in the response
+        // "session-id": "sessionId
       },
       body: JSON.stringify({ prompt: prompt?.toLowerCase() }),
     }
   );
 
   const data = await res.json();
+  //Waiting API to have sessionId parameter in the response
+  // const key = "sessionId";
+  // delete data[key];
 
   return data;
 }
