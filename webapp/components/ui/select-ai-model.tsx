f55ba97d517a32eaefebf46d9848e0091cc50b75
Loris Dié <loris.die@converteo.com>
2024-03-28 16:57:54 +0100

    [webapp] model selector ready

--- webapp/components/ui/select-ai-model.tsx
+++ webapp/components/ui/select-ai-model.tsx
@@ -7,12 +7,18 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useWindowSize } from "react-use";
+import { useSessionStorage } from "react-use";
 
 export default function SelectAiModel() {
   const { width } = useWindowSize();
+  const [model, setModel] = useSessionStorage("ai_model", "general");
+
+  const handleModelChange = (value: string) => {
+    setModel(value);
+  };
 
   return (
-    <Select defaultValue="general">
+    <Select defaultValue={model} onValueChange={handleModelChange}>
       <SelectTrigger className="w-auto gap-1">
         {width > 767 && <span>Model:</span>}
         {width > 767 && <SelectValue />}
--- webapp/data/data.ts
+++ webapp/data/data.ts
@@ -1,22 +1,26 @@
 let session_id = "";
-const url =
-  "https://augmented-chatbot-demo-4o52ykz34a-ew.a.run.app/processPrompt";
+const url = {
+  general:
+    "https://augmented-chatbot-demo-4o52ykz34a-ew.a.run.app/processPrompt",
+  ga: "",
+};
 const key = "sessionId";
 
 export async function getData(prompt: string | undefined) {
   const chatHistory = sessionStorage.getItem("chat_history");
-  if (chatHistory === null) {
-    const res = await fetch(
-      url,
+  const ai_model = sessionStorage.getItem("ai_model");
+  const cleanedAiModel = ai_model?.replace(/['"]+/g, "");
 
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt: prompt?.toLowerCase() }),
-      }
-    );
+  const selectedUrl = url[cleanedAiModel as keyof typeof url];
+
+  if (chatHistory === null) {
+    const res = await fetch(selectedUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: prompt?.toLowerCase() }),
+    });
 
     const data = await res.json();
     session_id = data.sessionId;
@@ -26,18 +30,14 @@ export async function getData(prompt: string | undefined) {
     return data;
   }
 
-  const res = await fetch(
-    url,
-
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        sessionId: session_id,
-      },
-      body: JSON.stringify({ prompt: prompt?.toLowerCase() }),
-    }
-  );
+  const res = await fetch(selectedUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      sessionId: session_id,
+    },
+    body: JSON.stringify({ prompt: prompt?.toLowerCase() }),
+  });
 
   const data = await res.json();
 
